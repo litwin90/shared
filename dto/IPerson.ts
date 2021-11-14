f5141057be1739b44a74f536248a874fd85063c6
Dzianis Davydau <dzianis_davydau@epam.com>
2021-11-14 13:48:35 +0100

    added shared lib

---
+++ dto/IPerson.ts
@@ -0,0 +1,15 @@
+export interface IPerson {
+  name: string;
+  proposalStatus: string;
+  englishLevel: string;
+  ["Some Other Property"]: string;
+  ["Some Other Property1"]: string;
+  ["Some Other Property2"]: string;
+  ["Some Other Property3"]: string;
+  ["Some Other Property4"]: string;
+  ["Some Other Property5"]: string;
+  ["Some Other Property6"]: string;
+  ["Some Other Property7"]: string;
+  ["Some Other Property8"]: string;
+  prop?: string;
+}
